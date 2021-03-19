192aad5ba0d33d9ba619de7f1ce0a878ac722aa4
Piotr Solnica <piotr.solnica@gmail.com>
2021-03-19 14:15:18 +0100

    new cli wip 3

--- lib/hanami/cli/commands.rb
+++ lib/hanami/cli/commands.rb
@@ -1,3 +1,14 @@
+module Hanami
+  module CLI
+    class Command < Dry::CLI::Command
+      def application
+        require "hanami/init"
+        Hanami.application
+      end
+    end
+  end
+end
+
 require_relative "application/commands/db/create"
 require_relative "application/commands/db/create_migration"
 require_relative "application/commands/db/drop"
